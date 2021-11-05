bc8aa18d6c20d8a3843b69010b0e7d5fe75f374b
Anthony Lebro <antho.devjam@gmail.com>
2021-11-05 16:31:25 +0100

    V1.3

--- db/migrate/20211103142622_create_articles.rb
+++ db/migrate/20211103142622_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.string :contents
+      t.link :image
       t.timestamps
     end
   end
